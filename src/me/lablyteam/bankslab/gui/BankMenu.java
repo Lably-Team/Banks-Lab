4a02fdbd5c3feaaa8ebf1879b78e22331763155c
iAtog <atogmc58vani@gmail.com>
2021-08-28 21:16:10 -0400

    Maked all bank menu customizable (includes items)

--- src/me/lablyteam/bankslab/gui/BankMenu.java
+++ src/me/lablyteam/bankslab/gui/BankMenu.java
@@ -15,11 +15,14 @@ import me.lablyteam.bankslab.account.Account;
 import me.lablyteam.bankslab.account.logging.AccountLog;
 import me.lablyteam.bankslab.builders.ItemBuilder;
 import me.lablyteam.bankslab.builders.SkullItemBuilder;
+import me.lablyteam.bankslab.enums.AccountMethod;
 import me.lablyteam.bankslab.lib.Utility;
+import me.lablyteam.bankslab.lib.YamlFile;
 
 public class BankMenu {
 	
 	private BanksLabMain main;
+	private YamlFile itemsFile;
 	
 	private ItemBuilder withdrawItem;
 	private ItemBuilder depositItem;
@@ -28,16 +31,25 @@ public class BankMenu {
 	
 	public BankMenu(BanksLabMain main) {
 		this.main = main;
+		this.itemsFile = main.getFiles().getItems();
 		
-		this.closeItem = new ItemBuilder(Material.BARRIER)
-				.setName("§cClose");
-		this.withdrawItem = new ItemBuilder(Material.DISPENSER)
-				.setName("§cWithdraw")
-				.setLore("", "§7Withdraw money from your bank","","§e§lCLICK RIGHT§7 for withdraw all");
-		this.depositItem = new ItemBuilder(Material.CHEST)
-				.setName("§aDeposit")
-				.setLore("", "§7Deposit money to your bank","","§e§lCLICK RIGHT§7 for deposit all");
-		this.glass = new ItemBuilder(Material.STAINED_GLASS_PANE, (short)7)
+		// In a future i can upgrade this code, please don't fuck me with that code :c
+		this.closeItem = new ItemBuilder(Material.matchMaterial(itemsFile.getString("items.close.material")))
+				.addFlags(ItemFlag.values())
+				.setName(Utility.colorize(itemsFile.getString("items.close.name", "&cClose")));
+		
+		this.withdrawItem = new ItemBuilder(Material.matchMaterial(itemsFile.getString("items.withdraw.material")))
+				.addFlags(ItemFlag.values())
+				.setName(Utility.colorize(itemsFile.getString("items.withdraw.name", "&cWithdraw")))
+				.setLore(colorizeLore(itemsFile.getStringList("items.withdraw.lore")));
+		
+		this.depositItem = new ItemBuilder(Material.matchMaterial(itemsFile.getString("items.deposit.material")))
+				.addFlags(ItemFlag.values())
+				.setName(Utility.colorize(itemsFile.getString("items.deposit.name", "&aDeposit")))
+				.setLore(colorizeLore(itemsFile.getStringList("items.deposit.lore")));
+		
+		this.glass = new ItemBuilder(Material.STAINED_GLASS_PANE, (short)itemsFile.getInt("items.glass.data_id"))
+				.addFlags(ItemFlag.values())
 				.setName("§7")
 				.build();
 	}
@@ -54,17 +66,17 @@ public class BankMenu {
 		String currentBalance = " §7Current balance: §6"+balance;
 		
 		SkullItemBuilder bankStatus = new SkullItemBuilder();
-		bankStatus.setName("§bBank status");
+		bankStatus.setName(Utility.colorize(itemsFile.getString("items.bank_status.name", "&bBank status")));
 		bankStatus.setLore("", currentBalance,"§7Bank: §b"+account.getBank().getDisplayName());
 		bankStatus.setOwner(player.getName());
 		
 		for(int i = 0; i < inventory.getSize(); i++) {
 			inventory.setItem(i, glass);
 		}
-		String noLogs = "§7No logs found.";
+		String noLogs = Utility.colorize(main.getFiles().getLang().getString("no-logs"));
 		
-		ItemBuilder logItem = new ItemBuilder(Material.MAP)
-				.setName("§aRecent transactions")
+		ItemBuilder logItem = new ItemBuilder(Material.matchMaterial(itemsFile.getString("items.transactions.material")))
+				.setName(Utility.colorize(itemsFile.getString("items.transactions.name")))
 				.addFlags(ItemFlag.values());
 		if(account.getLogs().size() < 1) {
 			logItem.setLore(noLogs);
@@ -77,11 +89,18 @@ public class BankMenu {
 				if(lore.size() > max) {
 					break;
 				}
-				
+				String plus = (log.getMethod() == AccountMethod.DEPOSIT ? "§a+" : "§c-") + "§r";
 				String name = log.getMethod().getName() + "ed";
-				String amount = Utility.formatCurrency(log.getAmount());
-				String target = log.getPlayer().getName();
-				lore.add("§c"+target+" §7"+name+" §6"+amount);
+				String amount = Utility.formatCurrency(log.getAmount()).replace(',', '.');
+				//String target = log.getPlayer().getName();
+				long createdAt = log.createdAt();
+				String replaced = itemsFile.getString("items.transactions.log-format", "%plus% &6%money%, &7%method_name% &9%time% ago")
+						.replace("%plus%", plus)
+						.replace("%money%", String.valueOf(log.getAmount()))
+						.replace("%formatted_money%", amount)
+						.replace("%method_name%", name)
+						.replace("%time%", formatDate(createdAt));
+				lore.add(replaced);
 			}
 			if(lore.size() < 1) {
 				logItem.setLore(noLogs);
@@ -99,5 +118,27 @@ public class BankMenu {
 		inventory.setItem(4, bankStatus.build());
 		
 		player.openInventory(inventory);
+	}
+
+	public String formatDate(long time) {
+		long date = time - System.currentTimeMillis();
+		float inMinutes = (date / (1000 * 60));
+		float inHours = (date / (1000 * 60));
+		
+		if(inMinutes < 1) {
+			return "few moments";
+		}
+		
+		
+		
+		return inHours < 1 ? inMinutes + " minutes" : inHours + " hours";
+	}
+	
+	public List<String> colorizeLore(List<String> lore) {
+		List<String> newLore = new ArrayList<>();
+		for(String line : lore) {
+			newLore.add(Utility.colorize(line));
+		}
+		return newLore;
 	}
 }
